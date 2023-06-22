1e167333ff7f729db57e1e271e3d71dd3d1fa970
Yogesh Rai <yr44366@gmail.com>
2023-06-22 23:52:43 +0530

    created register page

---
+++ src/Pages/Register.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { Icon } from '@iconify/react';
+import styles from "./Styles.module.css";
+
+function Register() {
+
+  const [show, setShow] = useState<boolean>(false);
+
+  const showHandle = ():void => {
+    setShow(!show)
+  }
+
+  return (
+    <div className={styles["container"]}>
+        <div className={styles['form-wrapper']}>
+            <h2>Register</h2>
+            <form >
+                <input type="text" placeholder="username" required />
+                <input type="emai" placeholder="email" required />
+                <div className={styles['input-password-field']}>
+                  <input type={show ? "text" : "password"} placeholder="password" required />
+                    <span onClick={showHandle}>
+                      {show ? 
+                      <Icon icon="mdi:show" /> 
+                      : 
+                      <Icon icon="mdi:hide" />
+                      }
+                    </span>
+                </div>
+                <input style={{ display: 'none' }} type="file" id="file" />
+                <label htmlFor="file">
+                  <Icon icon="bxs:image-add" color='#EE4E35' width="24" height="24" />
+                  <span>Add an avatar</span>
+                </label>
+                <button>Sign Up</button>
+            </form>
+            <p>Already have an account? Login</p>
+        </div>
+    </div>
+  )
+}
+
+export default Register
