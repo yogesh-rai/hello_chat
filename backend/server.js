a0c9e4366bcdfd556e87b0054ac59892016c8a1d
Yogesh Rai <yr44366@gmail.com>
2023-06-30 21:49:36 +0530

    started building BE part

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res)=> {
+    res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
